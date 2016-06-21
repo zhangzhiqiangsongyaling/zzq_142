a95eb58a19e28350f5a50070bffbe34dc5eccc3d
KaiKaiAllen <584100034@qq.com>
2016-06-21 18:34:07 +0800

    送给啊强

--- readme.php
+++ readme.php
@@ -1,26 +1,2 @@
 <?php
-/* 
- * Copyright(c)2016 All rights reserved.
- * @Licenced  http://www.w3.org
- * @Author  allen
- * @Create on 2016-6-20 11:00:58
- * @Version 1.0
- */
-//命名空间的使用 使用namespace去声明空间 定义一个函数、类、常量存在的空间
-//声明空间必须放在脚本的第一行
-namespace Myspace;
-
-//var_dump("hello"); Fatal error: Cannot redeclare var_dump() 
-function var_dump($var = NULL) {
-	if (empty($var)) {
-		phpinfo(1);
-	} else {
-		echo $var;
-	}
-}
-
-header("Content-type:text/html;charset=utf-8");
-//声明空间 再去调用函数 应当加上空间名 不加 则默认是当前空间
-\Myspace\var_dump();
-//使用根空间的函数
-\var_dump("hello");
+echo "测试";
